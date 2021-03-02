fc7ae32d723dba2a6c5bc71089aca24326de8c42
jayjun <jayjun.ryu@kakaocorp.com>
2021-03-02 16:21:28 +0900

    초기 사용하지 않는 파일 삭제 env 파일 및 환경변수 사용

--- config/env.js
+++ config/env.js
@@ -90,6 +90,8 @@ function getClientEnvironment(publicUrl) {
         // which is why it's disabled by default.
         // It is defined here so it is available in the webpackHotDevClient.
         FAST_REFRESH: process.env.FAST_REFRESH !== 'false',
+
+        DEV_TEST:"testDEV"  //여기다가 환경변수 적어주면 적용되는듯 보임
       }
     );
   // Stringify all values so we can feed into webpack DefinePlugin
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import Root from "./client/Root";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import dotenv from "dotenv";
+dotenv.config();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(process.env.NODE_ENV);
+console.log(process.env);
+console.log(process.env.DEV_TEST);
+console.log(process.env.REACT_APP_DEV_TEST1);
+
+ReactDom.render(<Root />, document.getElementById("root"));
